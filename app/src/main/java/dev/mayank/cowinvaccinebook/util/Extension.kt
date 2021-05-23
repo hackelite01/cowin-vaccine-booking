89725711c0d20d3864586554a03a373d239652fd
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-05-23 14:53:51 +0530

    Done

---
+++ app/src/main/java/dev/mayank/cowinvaccinebook/util/Extension.kt
@@ -0,0 +1,51 @@
+package dev.mayank.cowinvaccinebook.util
+
+import android.util.Base64
+import android.view.View
+import com.squareup.moshi.Moshi
+import dev.mayank.cowinvaccinebook.data.model.ErrorResponse
+import retrofit2.HttpException
+import java.security.MessageDigest
+
+fun HttpException.toErrorResponse(): ErrorResponse? {
+    return try {
+        this.response()?.errorBody()?.source()?.let {
+            val moshiAdapter = Moshi.Builder().build().adapter(ErrorResponse::class.java)
+            moshiAdapter.fromJson(it)
+        }
+    } catch (exception: Exception) {
+        null
+    }
+}
+
+fun String.md5(): String {
+    return hashString(this, "MD5")
+}
+
+fun String.sha256(): String {
+    return hashString(this, "SHA-256")
+}
+
+private fun hashString(input: String, algorithm: String): String {
+    return MessageDigest
+        .getInstance(algorithm)
+        .digest(input.toByteArray())
+        .fold("", { str, it -> str + "%02x".format(it) })
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun String.decodeBase64String(): String {
+    val byteArray = Base64.decode(this, Base64.DEFAULT)
+    return String(byteArray)
+}
---
+++ app/src/main/java/dev/mayank/cowinvaccinebook/util/HeaderInterceptor.kt
@@ -0,0 +1,21 @@
+package dev.mayank.cowinvaccinebook.util
+
+import dev.mayank.cowinvaccinebook.data.preference.AppPreference
+import dev.mayank.cowinvaccinebook.repository.CowinAppRepository
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class HeaderInterceptor(private val appPreference: AppPreference): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val requestBuilder = originalRequest.newBuilder()
+            .header("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36")
+
+        appPreference.getBearerToken()?.let {
+            requestBuilder.header("Authorization", "Bearer $it")
+        }
+        val request = requestBuilder.build()
+        return chain.proceed(request)
+    }
+}
